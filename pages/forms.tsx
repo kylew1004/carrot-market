9afd24fc817e2a38ad6c91c6834dfb9784206575
kylew1004 <kylew1004@gmail.com>
2022-05-26 23:34:47 +0900

    react hook form validate

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,37 +1,65 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
-// Less code (c)
-// Better validation
-// Better Erros (set, clear, display)
-// Have control over inputs
-// Dont deal with events (c)
-// Easier Inputs (c)
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+  errors?: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setError,
+    setValue,
+    reset,
+    resetField,
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+  const onValid = (data: LoginForm) => {
     console.log("im valid bby");
   };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="Username"
       />
+      {errors.username?.message}
       <input
-        {...register("email", { required: true })}
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="Email"
       />
+      {errors.email?.message}
       <input
-        {...register("password", { required: true })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
       <input type="submit" value="Create Account" />
+      {errors.errors?.message}
     </form>
   );
 }
@@ -39,4 +67,15 @@ export default function Forms() {
 {/*
   preventDefault => 새로고침 방지
   register 함수는 input을 state와 연결시키는 역할을 한다. useForm에서 온다.
+
+  handleSubmit
+  이 함수는 form 유효성 검사가 성공하면 form 데이터를 수신합니다.
+  ex) < form onSubmit={handleSubmit(onSubmit, onError)} />
+
+  onSubmit (SubmitHandler) (성공적인 콜백)
+  (data: Object, e?: Event) => void
+
+  onError (SubmitErrorHandler) (오류 콜백)
+  (errors: Object, e?: Event) => void
+
 */}
